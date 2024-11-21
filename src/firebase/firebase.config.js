fdfc4b17eb7bb685c2049cf07908a13f33d78560
sajjad244 <mrsajjed@gmail.com>
2024-11-21 18:09:01 +0600

    env.local file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,13 +6,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDxgK02z1IX7YFRs6mEcCH9N7IZ71O4rg8",
-    authDomain: "winter-donation-b573a.firebaseapp.com",
-    projectId: "winter-donation-b573a",
-    storageBucket: "winter-donation-b573a.firebasestorage.app",
-    messagingSenderId: "130606910192",
-    appId: "1:130606910192:web:650dfabeb71c5b6d4ad035"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
